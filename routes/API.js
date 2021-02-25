46392e5ac277eec04b766a39038f8fa65fe33d3e
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-25 16:41:59 +0100

    todo updated

--- routes/API.js
+++ routes/API.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const contentful = require('../data/contentful-export-ie8hzkoeixts-master-2021-02-25T11-53-22.json')
-const obj = contentful;
 
-// TODO readfile and import needed fields
+// TODO filter contentful: relevant only objs
 
 router.get("/", function(req, res, next) {
-    res.send(obj);
+    res.send(contentful);
 });
 
 module.exports = router;
