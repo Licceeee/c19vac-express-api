06fdc5ea51b23f147fe794167685e75f3506f500
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-03-06 00:21:28 +0100

    port changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8050;
 
 app.use(express.json())
 app.use(cors());
