815e2ecb2008200c833f1c37ac51ccbe593b1bb1
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-03-09 15:50:08 +0100

    add api docs

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 
-const port = process.env.PORT || 8050;
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(cors());
 app.use('/static', express.static(__dirname + '/public'));
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 const vaccines = require('./routes/vaccines')
 
 app.use('/api/v1', vaccines)
 
+
+
+
+
+
 app.get('/', async (req, res) => {
   try {
-     res.send("Express is responding")
+     res.render('index', {
+        title: "C19VAC API DOCS",
+    })
    } catch(e) {
     res.sendStatus(500)
   }
